fd1614dce9cfd58d0bab0fea7663488d8fea0e4d
Juliana Velandia <jvelandia@umariana.edu.co>
2024-04-13 19:28:55 -0500

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,15 +11,15 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'dados';
-  dadoIzquierdo :string = '../assets/img/dice1.png';
-  dadoDerecho: string = '../assets/img/dice4.png';
+  dadoIzquierdo :string = '../src/assets/img/dice1.png';
+  dadoDerecho: string = '../src/assets/img/dice4.png';
   numero1: number = 0;
   numero2: number = 0;
-
+  
   lanzarDados(): void {
     this.numero1 = Math.round(Math.random() * 5) + 1;
     this.numero2 = Math.round(Math.random() * 5) + 1;
-    this.dadoIzquierdo = '../assets/img/dice' + this.numero1 + '.png';
-    this.dadoDerecho = '../assets/img/dice' + this.numero2 + '.png';
+    this.dadoIzquierdo = '../src/assets/img/dice' + this.numero1 + '.png';
+    this.dadoDerecho = '../src/assets/img/dice' + this.numero2 + '.png';
   }
 }
