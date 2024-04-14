a9a8f53b0f1642bef1149c0c3f1a6b754c5dcc6a
Juliana Velandia <jvelandia@umariana.edu.co>
2024-04-13 19:02:53 -0500

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,21 +5,21 @@ import { RouterOutlet } from '@angular/router';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,CommonModule],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'dados';
-  dadoIzquierdo ='../assets/img/dice1.png';
-  dadoDerecho ='../assets/img/dice4.png';
+  dadoIzquierdo = '../assets/img/dice1.png';
+  dadoDerecho = '../assets/img/dice4.png';
   numero1: number = 0;
   numero2: number = 0;
 
-  lanzarDados():void {
-this.numero1 = Math.round(Math.random()*5)+1;
-this.numero2 = Math.round(Math.random()*5)+1;
-this.dadoIzquierdo ='../assets/img/dice'+this.numero1+'.png';
-this.dadoDerecho ='../assets/img/dice'+this.numero2+'.png';
+  lanzarDados(): void {
+    this.numero1 = Math.round(Math.random() * 5) + 1;
+    this.numero2 = Math.round(Math.random() * 5) + 1;
+    this.dadoIzquierdo = '../assets/img/dice' + this.numero1 + '.png';
+    this.dadoDerecho = '../assets/img/dice' + this.numero2 + '.png';
   }
 }
