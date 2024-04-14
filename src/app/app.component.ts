f1671daeae25b1bc3b3649aa2ba66f5bf9f77f3b
Juliana Velandia <jvelandia@umariana.edu.co>
2024-04-13 19:00:11 -0500

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
@@ -19,6 +20,6 @@ export class AppComponent {
 this.numero1 = Math.round(Math.random()*5)+1;
 this.numero2 = Math.round(Math.random()*5)+1;
 this.dadoIzquierdo ='../assets/img/dice'+this.numero1+'.png';
-this.dadoDerecho ='../assets/img/dice'+this.numero1+'.png';
+this.dadoDerecho ='../assets/img/dice'+this.numero2+'.png';
   }
 }
